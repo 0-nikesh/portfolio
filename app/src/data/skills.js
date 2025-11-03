8883b749f3af5fa67ef095eb2e4877192c61295d
Nikesh Bhandari <bhandarinikesh93@gmail.com>
2025-11-03 23:16:03 +0545

    skills js.

---
+++ app/src/data/skills.js
@@ -0,0 +1,56 @@
+export const skillCategories = [
+  {
+    id: 'programming',
+    emoji: '🖥️',
+    label: 'Programming & Scripting',
+    skills: ['JavaScript', 'TypeScript', 'Dart', 'Python', 'HTML5', 'CSS3']
+  },
+  {
+    id: 'frameworks',
+    emoji: '⚙️',
+    label: 'Frameworks & Libraries',
+    skills: ['React.js', 'Node.js', 'Express.js', 'Flutter', 'Django REST Framework', 'Vite.js', 'Tailwind CSS']
+  },
+    {
+    id: 'devtools',
+    emoji: '🧰',
+    label: 'Dev Tools & Platforms',
+    skills: ['Git', 'GitHub', 'Postman', 'Trello', 'VSCode', 'Android Studio','Visual Paradigm']
+  },
+  {
+    id: 'databases',
+    emoji: '🧱',
+    label: 'Databases & Backend',
+    skills: ['MongoDB', 'Firebase', 'PostgreSQL', 'Database Design', 'Normalization (1NF–3NF)', 'ER Diagrams', 'API Architecture']
+  },
+    {
+    id: 'uiux',
+    emoji: '🧩',
+    label: 'UI/UX Design & Prototyping',
+    skills: ['Figma', 'Wireframing', 'User Flow Mapping', 'High-Fidelity Prototyping', 'Usability Testing', 'Design Systems', 'Guerrilla Testing']
+  },
+  {
+    id: 'api',
+    emoji: '🔗',
+    label: 'API Development & Integration',
+    skills: ['RESTful API Design', 'JWT Authentication', 'CRUD Operations', 'Middleware', 'SMTP Integration', 'WebSocket', 'Postman API Testing']
+  },
+  {
+    id: 'aiml',
+    emoji: '🧠',
+    label: 'AI & Machine Learning',
+    skills: ['Google Gemini API', 'Natural Language Processing (NLP)', 'Optical Character Recognition (OCR)', 'Large Language Models (LLM)', 'Prompt Engineering', 'AI Summarization', 'Text-to-Speech (TTS)', 'Speech Recognition', 'OpenCV']
+  },
+  {
+    id: 'product',
+    emoji: '🧮',
+    label: 'Product Development & Management',
+    skills: ['Agile Methodologies', 'Scrum Framework', 'Scrum Master Experience',  'Sprint Planning', 'Product Lifecycle Management', 'User Story Creation', 'Backlog Grooming', 'Requirement Analysis']
+  },
+  {
+    id: 'soft',
+    emoji: '💼',
+    label: 'Soft & Business Skills',
+    skills: ['Product Thinking & Ideation', 'Presentation & Pitching', 'Shark Tank Runner-Up', 'Hackathon Winner', 'Problem Solving & Critical Thinking', 'Leadership & Team Coordination', 'Communication & Documentation', 'Business Case Study Development', 'Client Requirement Gathering']
+  }
+]
